ff0b2443a4bb1ec03fdfeb8ee24b9a5f28c8639c
李泓璞 <lhp950730@163.com>
2026-01-23 14:58:29 +0800

    feat: env

--- setup.sh
+++ setup.sh
@@ -4,10 +4,19 @@ set -e
 
 echo "===== 前端 setup.sh 开始 ====="
 
+# 默认参数
+ENV=${1:-prod}
+
 echo ">>> 1. 执行 ESLint"
 pnpm run lint
 
-
-pnpm run build
+# 3. 按环境构建
+if [[ "$ENV" == "test" ]]; then
+  echo ">>> 构建测试环境包（vite --mode test）"
+  pnpm run build:test
+else
+  echo ">>> 构建生产环境包"
+  pnpm run build
+fi
 
 echo "===== 前端 setup.sh 完成 ====="
