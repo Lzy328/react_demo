94fbf55dd83ce7a5babd294d79581f9ebc83db30
李泓璞 <lhp950730@163.com>
2026-01-27 16:48:57 +0800

    fix: -

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import { visualizer } from 'rollup-plugin-visualizer'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+      react(),
+  ],
   base: './',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
+  },
+  build: {
+    sourcemap: false,
   },
   server: {
     port: 5173,
